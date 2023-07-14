254f78bd0364368be96be35a098ade5e483e4964
Shintaro Sakoda <sakoda@keio.jp>
2023-07-15 05:32:08 +0900

    Added init
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- ros2/orienter_net_pkg/orienter_net_pkg/orienter_net_node.py
+++ ros2/orienter_net_pkg/orienter_net_pkg/orienter_net_node.py
@@ -1,11 +1,13 @@
 import rclpy
 from rclpy.node import Node
+from maploc.demo import Demo
 
 
 class OrienterNetNode(Node):
     def __init__(self):
-        super().__init__("mile_node")
+        super().__init__("orienter_net_node")
         self.get_logger().info(f"Initializing ...")
+        self.demo = Demo(num_rotations=256, device='cpu')
         self.get_logger().info(f"Ready")
 
 
